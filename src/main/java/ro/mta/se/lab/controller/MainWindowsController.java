c1de680d10164042c35bc5f436932ede4c29dbd1
Ciobanu Cosmin <ciobanu.cosmin998@gmail.com>
2021-01-08 17:23:59 +0200

    Schelet program initial
    Programul contine fisierul gitignore
    si are cheletul unei aplicatii JavaFX
    facuta cu Maven.
    
    Signed-off-by: Ciobanu Cosmin <ciobanu.cosmin998@gmail.com>

---
+++ src/main/java/ro/mta/se/lab/controller/MainWindowsController.java
@@ -0,0 +1,30 @@
+package ro.mta.se.lab.controller;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainWindowsController {
+
+    @FXML
+    private Label Label1;
+
+    @FXML
+    private void initialize(){
+        Label1.setText("Da");
+    }
+}
